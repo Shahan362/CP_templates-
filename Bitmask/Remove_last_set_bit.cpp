a271a4545fde5b1446cf2d20cc6df31993df0bce
Shahan362 <102469751+Shahan362@users.noreply.github.com>
2024-08-03 13:49:16 +0600

    Add files via upload

---
+++ Bitmask/Remove_last_set_bit.cpp
@@ -0,0 +1,38 @@
+//on the name of Allah:)
+#include<bits/stdc++.h>
+#define int         long long
+#define endl        "\n"
+#define pi          2 * acos(0.0)
+#define mod         1000000007
+#define Mul(a,b)    (a%mod * b%mod)%mod
+#define Add(a,b)    (a%mod + b%mod)%mod
+#define all(x)      (x).begin(),(x).end()
+#define allr(x)     (x).rbegin(),(x).rend()
+#define gcd(x, y)   (__gcd(x, y))
+#define lcm(x, y)   ((x/gcd(x, y))*y)
+#define faster      cin.tie(NULL), cout.tie(NULL);
+#define TC          int t ; cin>>t ; while (t--)
+const int N = 1e9 + 7;
+using namespace std;
+
+
+void s()
+{   
+	int n;
+	cin >> n;
+	n = (n&(n-1));
+	cout << n << endl;
+}
+
+int32_t main()
+{   ios::sync_with_stdio(false);
+
+    TC
+    s();
+
+
+}
+
+
+
+
